4366bccce13f843aa6c567249ec59ec785b8d156
Bhagwan Giri <bhagwanmgiri@gmail.com>
2025-08-03 12:36:12 +0530

    test

--- src/main/java/com/example/cicd_workshop/DemoController.java
+++ src/main/java/com/example/cicd_workshop/DemoController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
 
+
+
     @GetMapping("")
     ResponseEntity<String> get(){
         return new ResponseEntity<>("App deployed!", HttpStatus.OK);
